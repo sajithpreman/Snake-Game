1d07777333b1b20d2d348d9dff5ecf5265947a65
sajithpreman <sajithpreman01@gmail.com>
2022-02-27 17:40:06 +0530

    snake movement fineshed

--- snake.js
+++ snake.js
@@ -1,10 +1,13 @@
 // variable diclaration
 
 var cvs = document.getElementById("canvas").getContext("2d");
+var sposx = 80;
+var sposy = 80;
 
 // onload function
 window.onload = function () {
-  mainGame();
+  addEventListener("keydown", inputControl);
+  setInterval(mainGame, 200);
 };
 
 // main game function
@@ -14,7 +17,7 @@ function mainGame() {
 
   //bg-color
 
-  cvs.fillstyle = "red";
+  cvs.fillStyle = "black";
   cvs.fillRect(0, 0, 400, 400);
 
   // CVS.moveTo(20, 0);
@@ -32,6 +35,41 @@ function mainGame() {
   }
   cvs.strokeStyle = "gray";
   cvs.stroke();
+
+  //snake
+  cvs.fillStyle = "yellow";
+  cvs.fillRect(sposx, sposy, 20, 20);
+
+  //fruit
+
+  cvs.fillStyle = "red";
+  cvs.fillRect(140, 140, 20, 20);
 }
 
 //input controll
+
+function inputControl(e) {
+  console.log(e.keyCode);
+  console.log(e.key);
+  switch (e.keyCode) {
+    case 37:
+      //left
+      sposx -= 20;
+      break;
+
+    case 39:
+      //right
+      sposx += 20;
+      break;
+
+    case 38:
+      //up
+      sposy -= 20;
+      break;
+
+    case 40:
+      //down
+      sposy += 20;
+      break;
+  }
+}
